507938f6707c90565861979d17cd8bb88df44df5
theydy <ydythebs@qq.com>
2021-05-09 23:29:53 +0800

    feat: 第二章

---
+++ src/components/base/scroll/scroll.vue
@@ -0,0 +1,33 @@
+<template>
+  <div ref="rootRef">
+    <slot />
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+import useScroll from './use-scroll'
+
+export default {
+  name: 'scroll',
+  props: {
+    click: {
+      type: Boolean,
+      default: true
+    },
+    probeType: {
+      type: Number,
+      default: 0
+    }
+  },
+  setup (props, { emit }) {
+    const rootRef = ref(null)
+    const scroll = useScroll(rootRef, props, emit)
+
+    return {
+      rootRef,
+      scroll
+    }
+  }
+}
+</script>
--- src/views/recommend.vue
+++ src/views/recommend.vue
@@ -1,10 +1,90 @@
 <template>
-  <div class="recommend">
-    recommend
+  <div class="recommend" v-loading="loading">
+    <scroll class="recommend-content">
+      <div>
+        <div class="slider-wrapper">
+          <div class="slider-content">
+            <slider v-if="sliders.length" :sliders="sliders" />
+          </div>
+        </div>
+        <div class="recommend-list">
+          <h1 class="list-title" v-show="!loading">热门歌单推荐</h1>
+          <ul>
+            <li
+              v-for="item in albums"
+              class="item"
+              :key="item.id"
+              @click="selectItem(item)"
+            >
+              <div class="icon">
+                <img width="60" height="60" v-lazy="item.pic">
+              </div>
+              <div class="text">
+                <h2 class="name">
+                  {{ item.username }}
+                </h2>
+                <p class="title">
+                  {{ item.title }}
+                </p>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </scroll>
   </div>
 </template>
 
 <script>
+import { getRecommend } from '@/service/recommend'
+import Slider from '@/components/base/slider/slider'
+import Scroll from '@/components/base/scroll/scroll'
+import storage from 'good-storage'
+import { ALBUM_KEY } from '@/assets/js/constant'
+
+export default {
+  name: 'recommand',
+  components: {
+    Slider,
+    Scroll
+  },
+  data () {
+    return {
+      sliders: [],
+      albums: [],
+      selectedAlbum: null
+    }
+  },
+  computed: {
+    loading () {
+      return !this.sliders.length && !this.albums.length
+    }
+  },
+  async created () {
+    this.getRecommend()
+  },
+  methods: {
+    async getRecommend () {
+      try {
+        const { sliders, albums } = await getRecommend()
+        this.sliders = sliders
+        this.albums = albums
+      } catch (e) {
+        console.log(e)
+      }
+    },
+    selectItem (album) {
+      this.selectedAlbum = album
+      this.cacheAlbum(album)
+      this.$router.push({
+        path: `/recommend/${album.id}`
+      })
+    },
+    cacheAlbum (album) {
+      storage.session.set(ALBUM_KEY, album)
+    }
+  }
+}
 </script>
 
 <style lang="less" scoped>
